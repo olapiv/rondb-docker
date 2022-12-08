e259a1f6864caf13faf70020e6082ca3c76ff8f9
Vincent Lohse <vincent.lohse@logicalclocks.com>
2022-12-08 14:46:32 +1100

    Renamed "API" to "Bench" node; Renamed "node" to "container"; Added "REST container" flag

--- build_run_docker.sh
+++ build_run_docker.sh
@@ -28,14 +28,15 @@ set -e
 function print_usage() {
     cat <<EOF
 Usage: $0    
-    [-h         --help                              ]
-    [-v         --rondb-version             <string>]
-    [-ruri      --rondb-tarball-uri         <string>]
-    [-m         --num-mgm-nodes             <int>   ]
-    [-g         --node-groups               <int>   ]
-    [-r         --replication-factor        <int>   ]
-    [-my        --num-mysql-nodes           <int>   ]
-    [-a         --num-api-nodes             <int>   ]
+    [-h         --help                                  ]
+    [-v         --rondb-version                 <string>]
+    [-ruri      --rondb-tarball-uri             <string>]
+    [-m         --num-mgm-containers            <int>   ]
+    [-g         --node-groups                   <int>   ]
+    [-r         --replication-factor            <int>   ]
+    [-my        --num-mysql-containers          <int>   ]
+    [-ra        --num-rest-api-containers       <int>   ]
+    [-bc        --num-benchmarking-containers   <int>   ]
     [-b         --run-benchmark             <string>
                     Options: <sysbench_single, sysbench_multi, dbt2_single, dbt2_multi>
                                                     ]
@@ -54,9 +55,10 @@ fi
 
 # Defaults
 RONDB_TARBALL_LOCAL_REMOTE=remote
-NUM_MGM_NODES=1
-NUM_MYSQL_NODES=0
-NUM_API_NODES=0
+NUM_MGM_CONTAINERS=1
+NUM_MYSQL_CONTAINERS=0
+NUM_REST_API_NODES=0
+NUM_REST_CONTAINERS=0
 REPLICATION_FACTOR=1
 NODE_GROUPS=1
 RUN_BENCHMARK=
@@ -80,8 +82,8 @@ while [[ $# -gt 0 ]]; do
         shift # past argument
         shift # past value
         ;;
-    -m | --num-mgm-nodes)
-        NUM_MGM_NODES="$2"
+    -m | --num-mgm-containers)
+        NUM_MGM_CONTAINERS="$2"
         shift # past argument
         shift # past value
         ;;
@@ -95,13 +97,18 @@ while [[ $# -gt 0 ]]; do
         shift # past argument
         shift # past value
         ;;
-    -my | --num-mysql-nodes)
-        NUM_MYSQL_NODES="$2"
+    -my | --num-mysql-containers)
+        NUM_MYSQL_CONTAINERS="$2"
         shift # past argument
         shift # past value
         ;;
-    -a | --num-api-nodes)
-        NUM_API_NODES="$2"
+    -ra | --num-rest-api-containers)
+        NUM_REST_API_NODES="$2"
+        shift # past argument
+        shift # past value
+        ;;
+    -bc | --num-benchmarking-containers)
+        NUM_BENCH_CONTAINERS="$2"
         shift # past argument
         shift # past value
         ;;
@@ -128,15 +135,16 @@ echo "#################"
 echo "Parsed arguments:"
 echo "#################"
 echo
-echo "RonDB version                 = ${RONDB_VERSION}"
-echo "RonDB tarball local/remote    = ${RONDB_TARBALL_LOCAL_REMOTE}"
-echo "RonDB tarball URI             = ${RONDB_TARBALL_URI}"
-echo "Number of management nodes    = ${NUM_MGM_NODES}"
-echo "Node groups                   = ${NODE_GROUPS}"
-echo "Replication factor            = ${REPLICATION_FACTOR}"
-echo "Number of mysql nodes         = ${NUM_MYSQL_NODES}"
-echo "Number of api nodes           = ${NUM_API_NODES}"
-echo "Run benchmark                 = ${RUN_BENCHMARK}"
+echo "RonDB version                     = ${RONDB_VERSION}"
+echo "RonDB tarball local/remote        = ${RONDB_TARBALL_LOCAL_REMOTE}"
+echo "RonDB tarball URI                 = ${RONDB_TARBALL_URI}"
+echo "Number of management containers   = ${NUM_MGM_CONTAINERS}"
+echo "Node groups                       = ${NODE_GROUPS}"
+echo "Replication factor                = ${REPLICATION_FACTOR}"
+echo "Number of MySQL containers        = ${NUM_MYSQL_CONTAINERS}"
+echo "Number of REST API containers     = ${NUM_REST_CONTAINERS}"
+echo "Number of benchmarking containers = ${NUM_BENCH_CONTAINERS}"
+echo "Run benchmark                     = ${RUN_BENCHMARK}"
 echo
 
 set -- "${POSITIONAL[@]}" # restore unknown options
@@ -149,7 +157,7 @@ if [[ -n $1 ]]; then
     exit 1
 fi
 
-if [ $NUM_MGM_NODES -lt 1 ]; then
+if [ $NUM_MGM_CONTAINERS -lt 1 ]; then
     echo "At least 1 mgmd is required"
     exit 1
 elif [ $REPLICATION_FACTOR -lt 1 -o $REPLICATION_FACTOR -gt 4 ]; then
@@ -170,32 +178,32 @@ if [ ! -z $RUN_BENCHMARK ]; then
         "$RUN_BENCHMARK" != "dbt2_multi" ]; then
         echo "Benchmark has to be one of <sysbench_single, sysbench_multi, dbt2_single, dbt2_multi>"
         exit 1
-    elif [ $NUM_API_NODES -lt 1 ]; then
+    elif [ $NUM_BENCH_CONTAINERS -lt 1 ]; then
         echo "At least one api is required to run benchmarks"
         exit 1
-    elif [ $NUM_MYSQL_NODES -lt 1 ]; then
-        echo "At least one mysqld is required to run benchmarks"
+    elif [ $NUM_MYSQL_CONTAINERS -lt 1 ]; then
+        echo "At least one MySQLd is required to run benchmarks"
         exit 1
     fi
 
     # This is not a hard requirement, but is better for benchmarking
-    # One api container can however also run multiple Sysbench instances against multiple mysqld containers
+    # One api container can however also run multiple Sysbench instances against multiple MySQLd containers
     if [ "$RUN_BENCHMARK" == "sysbench_multi" ]; then
-        if [ $NUM_MYSQL_NODES -lt $NUM_API_NODES ]; then
-            echo "For sysbench_multi, there should be at least as many mysqld as api containers"
+        if [ $NUM_MYSQL_CONTAINERS -lt $NUM_BENCH_CONTAINERS ]; then
+            echo "For sysbench_multi, there should be at least as many MySQLd as api containers"
             exit 1
         fi
     fi
 
     if [ "$RUN_BENCHMARK" == "sysbench_multi" -o "$RUN_BENCHMARK" == "dbt2_multi" ]; then
-        if [ $NUM_MYSQL_NODES -lt 2 ]; then
-            echo "At least two mysqlds are required to run the multi-benchmarks"
+        if [ $NUM_MYSQL_CONTAINERS -lt 2 ]; then
+            echo "At least two MySQLds are required to run the multi-benchmarks"
             exit 1
         fi
     fi
 
     if [ "$RUN_BENCHMARK" == "dbt2_single" -o "$RUN_BENCHMARK" == "dbt2_multi" ]; then
-        if [ $NUM_API_NODES -gt 1 ]; then
+        if [ $NUM_BENCH_CONTAINERS -gt 1 ]; then
             echo "Can only run dbt2 benchmarks with one api container"
             exit 1
         fi
@@ -203,7 +211,7 @@ if [ ! -z $RUN_BENCHMARK ]; then
 
     # TODO: Make this work with BENCHMARK_SERVERS in sysbench_multi; This requires some
     #   care in synchronizing the api nodes when executing the benchmark.
-    if [ $NUM_API_NODES -gt 1 ]; then
+    if [ $NUM_BENCH_CONTAINERS -gt 1 ]; then
         echo "Running more than one api container for Sysbench benchmarks is currently not supported"
         exit 1
     fi
@@ -216,7 +224,7 @@ RONDB_VERSION_NO_DOT=$(echo "$RONDB_VERSION" | tr -d '.')
 # yes | docker container prune
 # yes | docker volume prune
 
-FILE_SUFFIX="v${RONDB_VERSION_NO_DOT}_m${NUM_MGM_NODES}_g${NODE_GROUPS}_r${REPLICATION_FACTOR}_my${NUM_MYSQL_NODES}_api${NUM_API_NODES}"
+FILE_SUFFIX="v${RONDB_VERSION_NO_DOT}_m${NUM_MGM_CONTAINERS}_g${NODE_GROUPS}_r${REPLICATION_FACTOR}_my${NUM_MYSQL_CONTAINERS}_ra${NUM_REST_CONTAINERS}_b${NUM_BENCH_CONTAINERS}"
 
 # https://stackoverflow.com/a/246128/9068781
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
@@ -239,7 +247,7 @@ rm -rf $SYSBENCH_SINGLE_DIR $DBT2_SINGLE_DIR \
     $SYSBENCH_MULTI_DIR $DBT2_MULTI_DIR
 
 mkdir -p $SYSBENCH_SINGLE_DIR $DBT2_SINGLE_DIR
-if [ "$NUM_MYSQL_NODES" -gt 1 ]; then
+if [ "$NUM_MYSQL_CONTAINERS" -gt 1 ]; then
     mkdir -p $SYSBENCH_MULTI_DIR $DBT2_MULTI_DIR
 fi
 
@@ -251,7 +259,7 @@ AUTOBENCH_DBT2_MULTI_FILEPATH="$DBT2_MULTI_DIR/autobench.conf"
 # Since we are mounting the entire benchmarking directories, these files would be 
 # overwritten if they are added via the Dockerfile.
 cp "$SCRIPT_DIR/resources/config_templates/dbt2_run_1.conf.single" "$DBT2_SINGLE_DIR/dbt2_run_1.conf"
-if [ "$NUM_MYSQL_NODES" -gt 1 ]; then
+if [ "$NUM_MYSQL_CONTAINERS" -gt 1 ]; then
     cp "$SCRIPT_DIR/resources/config_templates/dbt2_run_1.conf.multi" "$DBT2_MULTI_DIR/dbt2_run_1.conf"
 fi
 
@@ -316,7 +324,7 @@ BIND_CONFIG_INI_TEMPLATE="
         source: $CONFIG_INI_FILEPATH
         target: /srv/hops/mysql-cluster/config.ini"
 
-# Bind my.cnf to mysqld containers
+# Bind my.cnf to MySQLd containers
 BIND_MY_CNF_TEMPLATE="
       - type: bind
         source: $MY_CNF_FILEPATH
@@ -389,7 +397,7 @@ BASE_DOCKER_COMPOSE_FILE="version: '3.8'
 
 services:"
 
-for CONTAINER_NUM in $(seq $NUM_MGM_NODES); do
+for CONTAINER_NUM in $(seq $NUM_MGM_CONTAINERS); do
     NODE_ID=$((65 + $(($CONTAINER_NUM - 1))))
 
     template="$RONDB_DOCKER_COMPOSE_TEMPLATE"
@@ -473,8 +481,8 @@ for CONTAINER_NUM in $(seq $NUM_DATA_NODES); do
     CONFIG_INI=$(printf "%s\n\n%s" "$CONFIG_INI" "$SLOT")
 done
 
-if [ $NUM_MYSQL_NODES -gt 0 ]; then
-    for CONTAINER_NUM in $(seq $NUM_MYSQL_NODES); do
+if [ $NUM_MYSQL_CONTAINERS -gt 0 ]; then
+    for CONTAINER_NUM in $(seq $NUM_MYSQL_CONTAINERS); do
         template="$RONDB_DOCKER_COMPOSE_TEMPLATE"
         SERVICE_NAME="mysqld_$CONTAINER_NUM"
         template=$(echo "$template" | sed "s/<insert-service-name>/$SERVICE_NAME/g")
@@ -519,7 +527,7 @@ if [ $NUM_MYSQL_NODES -gt 0 ]; then
         env_var=$(printf "$ENV_VAR_TEMPLATE" "MYSQL_PASSWORD" "$MYSQL_PASSWORD")
         template+="$env_var"
         if [ $CONTAINER_NUM -eq 1 ]; then
-            # Only need one mysqld to setup databases, users, etc.
+            # Only need one MySQLd to setup databases, users, etc.
             env_var=$(printf "$ENV_VAR_TEMPLATE" "MYSQL_SETUP_APP" "1")
             template+="$env_var"
         fi
@@ -543,8 +551,8 @@ fi
 # Remove last semi-colon from MULTI_MYSQLD_IPS
 MULTI_MYSQLD_IPS=${MULTI_MYSQLD_IPS%?}
 
-if [ $NUM_API_NODES -gt 0 ]; then
-    for CONTAINER_NUM in $(seq $NUM_API_NODES); do
+if [ $NUM_BENCH_CONTAINERS -gt 0 ]; then
+    for CONTAINER_NUM in $(seq $NUM_BENCH_CONTAINERS); do
         template="$RONDB_DOCKER_COMPOSE_TEMPLATE"
         SERVICE_NAME="api_$CONTAINER_NUM"
         template=$(echo "$template" | sed "s/<insert-service-name>/$SERVICE_NAME/g")
@@ -559,7 +567,7 @@ if [ $NUM_API_NODES -gt 0 ]; then
             fi
 
             # Use the ndb_waiter to wait until RonDB has started before running benchmark
-            # Added extra sleep for mysqlds; may have to increase this
+            # Added extra sleep for MySQLds; may have to increase this
             command=$(printf "$COMMAND_TEMPLATE" ">
           bash -c \"ndb_waiter --ndb-connectstring=$MGM_CONNECTION_STRING &&
                     sleep 25 &&
@@ -581,10 +589,10 @@ if [ $NUM_API_NODES -gt 0 ]; then
 
         template+="$VOLUMES_FIELD"
         template+="$BIND_REST_API_JSON_TEMPLATE"
-        if [ "$NUM_MYSQL_NODES" -gt 0 ]; then
+        if [ "$NUM_MYSQL_CONTAINERS" -gt 0 ]; then
             template+="$BIND_SYS_SINGLE_DIR"
             template+="$BIND_DBT2_SINGLE_DIR"
-            if [ "$NUM_MYSQL_NODES" -gt 1 ]; then
+            if [ "$NUM_MYSQL_CONTAINERS" -gt 1 ]; then
                 template+="$BIND_SYS_MULTI_DIR"
                 template+="$BIND_DBT2_MULTI_DIR"
             fi
@@ -639,15 +647,15 @@ BASE_DOCKER_COMPOSE_FILE+="
 
 echo "Writing data to files"
 
-if [ "$NUM_MYSQL_NODES" -gt 0 ]; then
+if [ "$NUM_MYSQL_CONTAINERS" -gt 0 ]; then
     echo "Writing my.cnf"
     MY_CNF=$(printf "$MY_CNF_TEMPLATE" "$MYSQLD_SLOTS_PER_CONTAINER" "$MGM_CONNECTION_STRING")
     echo "$MY_CNF" >$MY_CNF_FILEPATH
 
-    if [ "$NUM_API_NODES" -gt 0 ]; then
-        echo "Writing benchmarking files for single mysqlds"
+    if [ "$NUM_BENCH_CONTAINERS" -gt 0 ]; then
+        echo "Writing benchmarking files for single MySQLds"
 
-        # This will always have 1 api and 1 mysqld container, and 1 Sysbench instance
+        # This will always have 1 api and 1 MySQLd container, and 1 Sysbench instance
         AUTOBENCH_SYSBENCH_SINGLE=$(printf "$AUTOBENCH_SYSBENCH_TEMPLATE" \
             "$SINGLE_MYSQLD_IP" "$MYSQL_USER" "$MYSQL_PASSWORD" \
             "$MYSQLD_SLOTS_PER_CONTAINER" "$MGMD_IPS" \
@@ -659,13 +667,13 @@ if [ "$NUM_MYSQL_NODES" -gt 0 ]; then
             "$MYSQLD_SLOTS_PER_CONTAINER" "$MGMD_IPS")
         echo "$AUTOBENCH_DBT2_SINGLE" >$AUTOBENCH_DBT2_SINGLE_FILEPATH
 
-        if [ "$NUM_MYSQL_NODES" -gt 1 ]; then
-            echo "Writing benchmarking files for multiple mysqlds"
+        if [ "$NUM_MYSQL_CONTAINERS" -gt 1 ]; then
+            echo "Writing benchmarking files for multiple MySQLds"
 
             AUTOBENCH_SYSBENCH_MULTI=$(printf "$AUTOBENCH_SYSBENCH_TEMPLATE" \
                 "$MULTI_MYSQLD_IPS" "$MYSQL_USER" "$MYSQL_PASSWORD" \
                 "$MYSQLD_SLOTS_PER_CONTAINER" "$MGMD_IPS" \
-                "$NUM_MYSQL_NODES")
+                "$NUM_MYSQL_CONTAINERS")
             echo "$AUTOBENCH_SYSBENCH_MULTI" >$AUTOBENCH_SYS_MULTI_FILEPATH
 
             AUTOBENCH_DBT2_MULTI=$(printf "$AUTOBENCH_DBT2_TEMPLATE" \
@@ -676,7 +684,7 @@ if [ "$NUM_MYSQL_NODES" -gt 0 ]; then
     fi
 fi
 
-if [ "$NUM_API_NODES" -gt 0 ]; then
+if [ "$NUM_BENCH_CONTAINERS" -gt 0 ]; then
     echo "Writing configuration file for the REST API server"
     REST_API_CONFIG=$(printf "$REST_API_CONFIG_TEMPLATE" "$MGMD_IPS" "$SINGLE_MYSQLD_IP" "$MYSQL_USER" "$MYSQL_PASSWORD")
     echo "$REST_API_CONFIG" >$REST_API_JSON_FILEPATH
